d3fe30000b866b1473113cb79e62ca2583444397
Ahmed <ahmed.abdelfattah.mossad.com>
2026-07-13 21:25:57 +0300

    Initialize FastAPI project with basic structure and endpoints

---
+++ README.md
@@ -0,0 +1,15 @@
+# FlyRank Backend AI Engineering
+
+## Assignment BE-01
+
+### Run
+
+pip install -r requirements.txt
+
+python -m uvicorn app:main.app --reload
+
+### Endpoints
+
+GET /
+
+GET /health
---
+++ app/main.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def home():
+    return {
+        "message": "Hello FlyRank"
+    }
+
+@app.get("/health")
+def health():
+    return {
+        "status": "ok"
+    }
